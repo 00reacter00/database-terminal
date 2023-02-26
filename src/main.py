cd63123f2291d17107cc5655489b945a603cee2b
00reacter00 <tkachenkov45@mail.ru>
2023-02-26 13:20:00 +0300

    Add connection to DB

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
+import pymysql
 from config import host, user, password, database
 
 if __name__ == '__main__':
-    pass
+    print(f"connecting to database '{database}'...")
+
+    try:
+        connection = pymysql.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+        print('successfully connected.')
+        print('=' * 20)
+
+        try:
+            # command functions & interpreter
+            pass
+        finally:
+            connection.close()  # close connection
+    except Exception as e:
+        print(f'failed to connect: {e}')
