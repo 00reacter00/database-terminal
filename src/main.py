207915d4b7ec48b486e9b7906b97875b8551cd1e
00reacter00 <tkachenkov45@mail.ru>
2023-02-26 17:00:44 +0300

    Add view_table()

--- src/main.py
+++ src/main.py
@@ -25,11 +25,23 @@ if __name__ == '__main__':
                     table_name (str): Name of the table,
                     columns (str): Columns of the table.
                 """                
-
                 with connection.cursor() as cursor: # connection.cursor => cursor
                     create_table_sql = f"CREATE TABLE `{table_name}`{columns};" # sql request
                     cursor.execute(create_table_sql)  # create table
                     print('create table successfully✅')
+
+            def view_table(table_name: str):
+                """Function to view table
+
+                Args:
+                    table_name (str): Name of the table.
+                """                
+                with connection.cursor() as cursor: # connection.cursor => cursor
+                    view_table_sql = f"SELECT * FROM `{table_name}`;" # sql request
+                    cursor.execute(view_table_sql)  # create table
+                    data = cursor.fetchall() # fetch data from DB
+                    for row in data: # table row output
+                        print(row)
         finally:
             connection.close()  # close connection
     except Exception as e:
